6bf314c0e7dca17d00b803c4484599042feb6ef4
ffuujian <ffuujian@163.com>
2016-12-11 15:51:19 +0800

    自定义类型demo的Client端

---
+++ AIDLClientSecond/src/main/aidl/com/example/aidlsecond/Person.aidl
@@ -0,0 +1,6 @@
+// Person.aidl
+package com.example.aidlsecond;
+
+// Declare any non-default types here with import statements
+
+parcelable Person;
---
+++ AIDLClientSecond/src/main/java/com/example/aidlsecondclient/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.aidlsecondclient;
+
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.example.aidlsecond.IMyAidlInterface;
+import com.example.aidlsecond.Person;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String TAG = "MainActivity";
+
+    private IMyAidlInterface mIMyAidlInterface;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.bt).setOnClickListener(this);
+        bindService();
+    }
+
+    private void bindService() {
+
+        Intent intent = new Intent();
+        intent.setComponent(new ComponentName("com.example.aidlsecond", "com.example.aidlsecond.MyService"));
+
+        bindService(intent, conn, BIND_AUTO_CREATE);
+    }
+
+    private ServiceConnection conn = new ServiceConnection() {
+
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            mIMyAidlInterface = IMyAidlInterface.Stub.asInterface(service);
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            mIMyAidlInterface = null;
+        }
+    };
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+
+            case R.id.bt:
+                try {
+                    List<Person> persons = mIMyAidlInterface.add(new Person("sam", 27));
+                    Log.d(TAG, "onClick: " + persons);
+                } catch (RemoteException e) {
+                    e.printStackTrace();
+                }
+                break;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unbindService(conn);
+    }
+}
